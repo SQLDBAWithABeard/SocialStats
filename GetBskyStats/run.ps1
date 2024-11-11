98bc36397d1383e9ce67f633babbf0eb86d2361a
jpomfret <jpomfret7@gmail.com>
2024-11-11 08:47:43 +0000

    output and a body for http out

--- GetBskyStats/run.ps1
+++ GetBskyStats/run.ps1
@@ -9,11 +9,15 @@ Write-Host "PowerShell HTTP trigger function processed a request."
 $stats = Invoke-RestMethod -Uri "https://public.api.bsky.app/xrpc/app.bsky.actor.getProfile?actor=jpomfret.bsky.social" -Method GET |
 Select-Object -property handle, displayName, avatar, createdAt, description, indexedAt, banner, followersCount, followsCount, postsCount
 
+$stats
+
 # Assign the value we want to pass to the SQL Output binding. 
 # The -Name value corresponds to the name property in the function.json for the binding
 Push-OutputBinding -Name socialstats -Value $stats
 
+
 # Associate values to output bindings by calling 'Push-OutputBinding'.
 Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
     StatusCode = [HttpStatusCode]::OK
+    Body = "Complete"
 })

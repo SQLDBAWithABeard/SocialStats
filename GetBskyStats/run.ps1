8c1babca1f571d5e0058d082710139befb155ade
jpomfret <jpomfret7@gmail.com>
2024-11-12 08:38:59 +0000

    remove http out

--- GetBskyStats/run.ps1
+++ GetBskyStats/run.ps1
@@ -15,8 +15,3 @@ $stats
 # The -Name value corresponds to the name property in the function.json for the binding
 Push-OutputBinding -Name socialstats -Value $stats
 
-# Associate values to output bindings by calling 'Push-OutputBinding'.
-Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
-    StatusCode = [HttpStatusCode]::OK
-    Body = $stats
-})

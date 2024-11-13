d639429949604f4e14e0dde1e0333d36bf4225f3
Rob Sewell <mrrobsewell@outlook.com>
2024-11-13 17:13:06 +0000

    so that we can do more htan one user

--- GetBskyStats/run.ps1
+++ GetBskyStats/run.ps1
@@ -6,12 +6,17 @@ param($dailyTimer)
 # Write to the Azure Functions log stream.
 Write-Host "PowerShell HTTP trigger function processed a request."
 
-$stats = Invoke-RestMethod -Uri "https://public.api.bsky.app/xrpc/app.bsky.actor.getProfile?actor=jpomfret.co.uk" -Method GET |
-Select-Object -property handle, displayName, avatar, createdAt, description, indexedAt, banner, followersCount, followsCount, postsCount
+$usernames = 'robsewell.com', 'tracisewell.com'
 
+$stats = foreach ($username in $usernames) {
+
+    $url = "https://public.api.bsky.app/xrpc/app.bsky.actor.getProfile?actor={0}" -f $username
+
+    Invoke-RestMethod -Uri $url -Method GET |
+    Select-Object -Property handle, displayName, avatar, createdAt, description, indexedAt, banner, followersCount, followsCount, postsCount
+
+}
 $stats
-
-# Assign the value we want to pass to the SQL Output binding. 
+# Assign the value we want to pass to the SQL Output binding.
 # The -Name value corresponds to the name property in the function.json for the binding
 Push-OutputBinding -Name socialstats -Value $stats
-
